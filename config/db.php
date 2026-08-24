38c142407ca5f66bd5afc3cd68f888534bbd2ea1
LURO08 <romeropalaciosjoseluis8@gmail.com>
2026-08-24 17:05:36 -0600

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,12 +1,11 @@
 <?php
 
-$host = getenv('PGHOST') ?: 'dpg-d9a9htgk1i2s73fa59lg-a.oregon-postgres.render.com';
-$user = getenv('PGUSER') ?: 'jlromero';
-$pass = getenv('PGPASSWORD') ?: '2G3XIajD62KAaUGIKysn1rfIypQ2FI2U';
-$dbname = getenv('PGDATABASE') ?: 'repuve_mantenimiento';
+$host = getenv('PGHOST') ?: 'dpg-da4ugt3m8hqs73arpaq0-a';
+$user = getenv('PGUSER') ?: 'repuvebd_user';
+$pass = getenv('PGPASSWORD') ?: 'TD9eVqoRqNPfOKjZQctCp0kzBZwJCKm7';
+$dbname = getenv('PGDATABASE') ?: 'repuvebd';
 $port = getenv('PGPORT') ?: '5432';
 $sslmode = getenv('PGSSLMODE') ?: 'require';
-
 
 try {
     $dsn = "pgsql:host={$host};port={$port};dbname={$dbname};sslmode={$sslmode}";
