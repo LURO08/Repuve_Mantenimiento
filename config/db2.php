f033f1609c7a1533834db5fd338af6d66cf52032
LURO08 <romeropalaciosjoseluis8@gmail.com>
2026-08-24 22:15:29 -0600

    7

--- config/db2.php
+++ config/db2.php
@@ -1,18 +1,11 @@
 <?php
 
-// $host = getenv('PGHOST') ?: 'localhost';
-// $user = getenv('PGUSER') ?: 'postgres';
-// $pass = getenv('PGPASSWORD') ?: '0806';
-// $dbname = getenv('PGDATABASE') ?: 'repuve_mantenimiento';
-// $port = getenv('PGPORT') ?: '5432';
-// $sslmode = getenv('PGSSLMODE') ?: 'disable';
-
 $host = getenv('PGHOST') ?: 'dpg-da4ugt3m8hqs73arpaq0-a';
 $user = getenv('PGUSER') ?: 'repuvebd_user';
 $pass = getenv('PGPASSWORD') ?: 'TD9eVqoRqNPfOKjZQctCp0kzBZwJCKm7';
 $dbname = getenv('PGDATABASE') ?: 'repuvebd';
 $port = getenv('PGPORT') ?: '5432';
-$sslmode = getenv('PGSSLMODE') ?: 'disable';
+$sslmode = getenv('PGSSLMODE') ?: 'require';
 
 try {
     $dsn = "pgsql:host={$host};port={$port};dbname={$dbname};sslmode={$sslmode}";
